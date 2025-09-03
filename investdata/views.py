d66ef5ff5d9aca47a0e4c15c5383b733a5bab748
Joao Batista <joaobldss@gmail.com>
2025-09-03 20:41:17 -0300

    feat: create conection on celery, rabbitmq e docker container

--- investdata/views.py
+++ investdata/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import response
+from django.http import JsonResponse
+from investdata.models import Stocks
+from investdata.tasks import get_stock_price
 
-# Create your views here.
+
+class StockPriceView(View):
+    def post(self, request):
+        stock_name = request.POST.get('stock_name')
+
+        get_stock_price.delay(stock_name)
+
+        return JsonResponse(
+            data={
+                'message': 'Tarefa disparada com sucesso!'
+            },
+            status=200,
+        )
