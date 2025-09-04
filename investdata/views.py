568b13540279827abd66749faeaf216389770581
Joao Batista <joaobldss@gmail.com>
2025-09-04 11:26:40 -0300

    feat: Adding django celery beat: to perform scheduled tasks automatically

--- investdata/views.py
+++ investdata/views.py
@@ -1,19 +1,20 @@
 from django.views import View
-from django.http import response
 from django.http import JsonResponse
 from investdata.models import Stocks
-from investdata.tasks import get_stock_price
+from investdata.tasks import get_stock_price_task
 
 
 class StockPriceView(View):
     def post(self, request):
         stock_name = request.POST.get('stock_name')
 
-        get_stock_price.delay(stock_name)
+        if not stock_name:
+            return JsonResponse({
+                'menssage':'stock_name não informado'
+            }, status=400)
 
-        return JsonResponse(
-            data={
-                'message': 'Tarefa disparada com sucesso!'
-            },
-            status=200,
-        )
+        get_stock_price_task.delay(stock_name)
+
+        return JsonResponse({
+            'message': 'Tarefa disparada com sucesso!'
+        })
