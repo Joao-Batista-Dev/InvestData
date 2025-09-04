969615012b93b3df8e628272f4342c86b4c09d67
Joao Batista <joaobldss@gmail.com>
2025-09-03 21:16:22 -0300

    feat: create aplication com django celery result, to save data automatically

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_results',
     'investdata',
 ]
 
@@ -112,7 +113,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CELERY_BROKER_URL = 'pyamqp://guest@rabbitmq//'
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 CELERY_TIMEZONE = 'America/Sao_Paulo'
-
+CELERY_RESULT_BACKEND = 'django-db'
 
