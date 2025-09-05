6dc8c109df6480fe7fcf865a8a7abcab157b5f4d
Joao Batista <joaobldss@gmail.com>
2025-09-05 11:28:38 -0300

    feat: Adding readmer to the project and making some corrections

--- core/settings.py
+++ core/settings.py
@@ -118,7 +118,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
 CELERY_TIMEZONE = 'America/Sao_Paulo'
 CELERY_RESULT_BACKEND = 'django-db'
 
